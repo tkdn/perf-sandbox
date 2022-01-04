191a9b6a3858d7fb726ea1a0eb341ce92dc5f33b
tkdn <tkdnation@gmail.com>
2022-01-04 16:34:54 +0900

    Angular: Root App コンポーネント整理

--- ng-sample/src/app/app.module.ts
+++ ng-sample/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,8 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
