beb2c4578a220ca7beb75875dfc9b38e138ce529
tkdn <tkdnation@gmail.com>
2022-01-04 16:35:51 +0900

    Angular: Routing 統合

--- ng-sample/src/app/app-routing.module.ts
+++ ng-sample/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonsWithCssComponent } from './component/pokemons-with-css/pokemons-with-css.component';
+import { PokemonsWithInlineStyleComponent } from './component/pokemons-with-inline-style/pokemons-with-inline-style.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pokemons-with-css',
+    component: PokemonsWithCssComponent,
+  },
+  {
+    path: 'pokemons-with-inline-style',
+    component: PokemonsWithInlineStyleComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- ng-sample/src/app/app.module.ts
+++ ng-sample/src/app/app.module.ts
@@ -4,8 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonsWithCssComponent } from './component/pokemons-with-css/pokemons-with-css.component';
+import { PokemonsWithInlineStyleComponent } from './component/pokemons-with-inline-style/pokemons-with-inline-style.component';
 
 @NgModule({
+  declarations: [
+    AppComponent,
+    PokemonsWithCssComponent,
+    PokemonsWithInlineStyleComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
